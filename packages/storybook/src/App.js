c83d3cf4d1a0b118cb192eedec69dd694f41009d
Denis ZHENG <zheng.denis@gmail.com>
2020-03-26 19:07:47 +0100

    init

---
+++ packages/storybook/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Button from "@beys/components";
+
+const App = () => {
+  return (
+    <View>
+      <Text>abdc</Text>
+      <Button />
+    </View>
+  );
+};
+
+export default App;
